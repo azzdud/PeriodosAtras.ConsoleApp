174d32fbaf06062f2103dba1c29881e6aaad0e4a
Luís Eduardo Bernardino Luz <dudubluz@gmail.com>
2022-03-24 16:34:17 -0300

    Add project files.

---
+++ PeriodosAtras.ConsoleApp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace PeriodosAtras.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            //Data data = new Data();
+            DateTime dataHJ = new DateTime();
+            DateTime dataInput = new DateTime();
+
+            Console.WriteLine("A data de hoje é: ");
+            dataHJ = Convert.ToDateTime(Console.ReadLine());
+
+            Console.WriteLine("Para saber um intervalo de tempo, digite a data em questão: ");
+            dataInput = Convert.ToDateTime(Console.ReadLine());
+            
+            Console.WriteLine("A diferença de dias é: " + (dataHJ - dataInput).Days + " dias.");
+
+            Console.ReadLine();
+        }
+    }
+
+    /*public class Data
+    {
+        public string strDataInput;
+        public DateTime dataInput;
+        public TimeSpan delta;
+        public int deltaDias;
+
+        public void EscreverDelta()
+        {
+
+        }
+
+        public DateTime Conversor(string strDataInput)
+        {
+            dataInput = Convert.ToDateTime(strDataInput);
+            
+            return dataInput;
+        }
+
+        public TimeSpan Calculadora()
+        {
+            delta = DateTime.Today - dataInput;
+
+            return default;
+        }
+    }*/
+}
